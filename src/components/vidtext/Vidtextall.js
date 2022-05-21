3b604dfd6ae4000eb107bf5efeab8c12017c3b6e
rismanobahar <rismanobahar@ce.student.pens.ac.id>
2022-05-21 18:27:43 +0700

    a

--- src/components/vidtext/Vidtextall.js
+++ src/components/vidtext/Vidtextall.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import Vidtext from './Vidtext'
 
 export default function Vidtextall() {
